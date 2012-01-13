4254122ac21742d46922f1c0a9ae2b0cabb672e6
Benedikt Pletzer <bpletzer@googlemail.com>
2012-01-13 10:39:19 +0100

    facebook redone

--- example/script/social/facebook.js
+++ example/script/social/facebook.js
@@ -1,19 +1,15 @@
-define(["jquery"], function($){
+define(['jquery'], function($){
   var exports = {};
 
   exports.init = function(elem){
-    var $elem = $(elem);
+    $(elem).removeClass('off');
 
-    $elem.attr("data-event-app-id", 153013318066950);
-    $elem.attr("class", "fb-live-stream");
-    $elem.attr("data-width", 400);
-    $elem.attr("data-height", 500);
-    $elem.attr("data-always-post-to-friends", "false");
-
-    $('body').append('<div id="fb-root"></div>');
+    $(document.body).append('<div id="fb-root"/>');
 
     require(["http://connect.facebook.net/de_DE/all.js"], function(){
-      FB.XFBML.parse(elem);
+      var element = elem;
+      FB.init({appId: '159909617367779'});
+      FB.XFBML.parse(element);
     });
 
   };
